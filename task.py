6e70998c0f7bc63b889f95b5efc926cd6b8b10af
maripierdo <88013657+maripierdo85@users.noreply.github.com>
2021-11-18 18:38:06 -0300

    Update task.py

--- task.py
+++ task.py
@@ -117,7 +117,7 @@ def minimal_task():
         path = f"output/amounts.xlsx"      
         open_the_website("https://itdashboard.gov/")
         #browser_lib.set_browser_implicit_wait(30)
-        browser_lib.wait_until_page_contains('DIVE IN')
+        #browser_lib.wait_until_page_contains('DIVE IN')
         click_button("//*[@id='node-23']/div/div/div/div/div/div/div/a")
         browser_lib.set_browser_implicit_wait(35)
         totals = agency_totals()
